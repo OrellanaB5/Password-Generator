09d432bdd7b3822e094044318ba6e26e837fe80b
Brandon <brandondenzell566@outlook.com>
2024-02-08 01:50:52 -0500

    added pseudo code

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//make function for password 
+
+//check if password is long enough
+
+//confirm if the charcters needed are included
+
+//check if at least one character type is selected
+
+//string variables containing the possible characters
+
+//var to store the characters
+
+//append character based on the user input
+
+//var to store the password
+
+//loop to generate the password
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
